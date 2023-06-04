f7f48e0164d7a747fd42ef6dde4565458febd21b
DahianaDiaz <114367346+DahianaDiaz@users.noreply.github.com>
2023-06-04 19:50:45 -0400

    Add files via upload

---
+++ AnaLex/AnaLex/AnalizadorLexico.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaLex
+{
+    class AnalizadorLexico
+    {
+        private LinkedList<Token> Salida;
+        private int estado;
+        private String auxlex;
+
+        public LinkedList<Token> escanear(String entrada)
+        {
+            entrada = entrada + "#";
+            Salida = new LinkedList<Token>();
+            estado = 0;
+            auxlex = "";
+            Char c;
+
+            for (int i =0; i <= entrada.Length-1; i++)
+            {
+                c = entrada.ElementAt(i);
+                switch (estado)
+                {
+                    case 0:
+                        if(Char.IsDigit(c))
+                        {
+                            estado = 1;
+                            auxlex += c;
+                        }
+                        else if (c.CompareTo('+') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.SIGNO_MAS);
+                        }
+                        else if (c.CompareTo('-') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.SIGNO_MEN);
+                        }
+                        else if (c.CompareTo('*') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.SIGNO_POR);
+                        }
+                        else if (c.CompareTo('/') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.SIGNO_DIV);
+                        }
+                        else if (c.CompareTo('^') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.SIGNO_POW);
+                        }
+                        else if (c.CompareTo('(') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.PARENTESIS_IZQ);
+                        }
+                        else if (c.CompareTo(')') == 0)
+                        {
+                            auxlex += c;
+                            agregarToken(Token.Tipo.PARENTESIS_DER);
+                        }
+                        else if(c.CompareTo('#')==0 && i == entrada.Length - 1)
+                        {
+                            Console.WriteLine("Hemos concluido el analisis con exito");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error lexico con:" + c);
+                            estado = 0;
+                        }
+                        break;
+                    case 1:
+                        if (char.IsDigit(c))
+                        {
+                            estado = 1;
+                            auxlex += c;
+
+                        }else if (c.CompareTo('.') == 0)
+                        {
+                            estado = 2;
+                            auxlex += c;
+                        }
+                        else
+                        {
+                            agregarToken(Token.Tipo.NUMERO_ENTERO);
+                            i -= 1;
+                        }
+                            break;
+                    case 2:
+                        if (char.IsDigit(c))
+                        {
+                            estado = 3;
+                            auxlex += c;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error lexico con:" + c);
+                            estado = 0;
+                        }
+                        break;
+                    case 3:
+                        if (char.IsDigit(c))
+                        {
+                            estado = 3;
+                            auxlex += c;
+                        }
+                        else
+                        {
+                            agregarToken(Token.Tipo.NUMERO_REAL);
+                            i -= 1;
+                        }
+                        break;
+                }
+            }
+            return Salida;
+
+        }
+
+        public void agregarToken(Token.Tipo tipo)
+        {
+            Salida.AddLast(new Token(tipo, auxlex));
+            auxlex = "";
+            estado = 0;
+
+        }
+        public void imprimirListaToken(LinkedList<Token>lista)
+        {
+            foreach(Token item in lista)
+            {
+                Console.WriteLine(item.GetTipo() + "<-->" + item.Getval());
+            }
+        }
+    }
+}
---
+++ AnaLex/AnaLex/Token.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaLex
+{
+    class Token
+    {
+        public enum Tipo
+        {
+            NUMERO_ENTERO,
+            NUMERO_REAL,
+            SIGNO_MAS,
+            SIGNO_MEN,
+            SIGNO_POR,
+            SIGNO_DIV,
+            SIGNO_POW,
+            PARENTESIS_IZQ,
+            PARENTESIS_DER
+        }
+        private Tipo tipoToken;
+        private String valor;
+        public Token(Tipo tipoDelToken, string val)
+        {
+            this.tipoToken = tipoDelToken;
+            this.valor = val;
+
+        }
+        public String Getval()
+        {
+            return valor;
+        }
+        public String GetTipo()
+        {
+            switch (tipoToken)
+            {
+                case Tipo.NUMERO_ENTERO:
+                    return "Numero Entero";
+                case Tipo.NUMERO_REAL:
+                    return "Numero Real";
+                case Tipo.SIGNO_MAS:
+                    return "Signo Mas";
+                case Tipo.SIGNO_MEN:
+                    return "Signo Menos";
+                case Tipo.SIGNO_POR:
+                    return "Signo Multiplicacion";
+                case Tipo.SIGNO_DIV:
+                    return "Signo Division";
+                case Tipo.SIGNO_POW:
+                    return "Signo Potencia";
+                case Tipo.PARENTESIS_IZQ:
+                    return "Parentesis Abre";
+                case Tipo.PARENTESIS_DER:
+                    return "Parentesis Cierra";
+                default:
+                    return "Desconocido";
+
+            }
+
+        }
+
+
+    }
+}
